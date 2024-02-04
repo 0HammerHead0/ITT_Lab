b3372ddfecb3e329a6ca4bf08079e5a22db34247
0HammerHead0 <shashwatakahammer@gmail.com>
2024-02-04 16:26:43 +0530

    Lab3 que2

--- Lab3/que2/style.js
+++ Lab3/que2/style.js
@@ -4,14 +4,19 @@ output.style.width = text.offsetWidth+'px';
 
 function convert(){
     let words=text.value.split(' ');
-    let res='';
-    words.forEach((word)=>{
-        firstChar = word[0];
-        let newWord = word.slice(1,word.length)
-        if()
-            newWord+=firstChar+'ay';
-            res+=newWord+' ';
-    })
+    let res='Nothing here yet';
+    console.log(words)
+    if(text.value!=''){
+        res='';
+        words.forEach((word)=>{
+            if(word!=''){
+                firstChar = word[0];
+                let newWord = word.slice(1,word.length)
+                newWord+=firstChar+'ay';
+                res+=newWord+' ';
+            }
+        })
+    }
     output.innerHTML=res
 }
 text.addEventListener('input',(event)=>{
