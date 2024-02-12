af48b033bae9e53a4a5c52cc5d64ccc813a4e91f
Shashwat Kumar Trivedi <125369875+0HammerHead0@users.noreply.github.com>
2024-02-12 14:47:19 +0530

    Add files via upload

---
+++ Lab6/a.py
@@ -0,0 +1,6 @@
+s = "hello"
+s_ = "hello"
+print(s==s_[::-1])
+s="helo"
+s_="oleh"
+print(s==s_[::-1])
---
+++ Lab6/c.py
@@ -0,0 +1,4 @@
+stringWithWords = "this is a string with multiple words"
+lis = stringWithWords.split(' ')
+lis.sort()
+print(' '.join(lis))
